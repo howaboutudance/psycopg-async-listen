be153a66587c170efebe996634d8a95f7ef843fe
Michael <mike@hematite.tech>
2024-09-24 18:39:25 -0700

    add main entry module to execute listener

--- src/psycopg_async_listen/__main__.py
+++ src/psycopg_async_listen/__main__.py
@@ -4,6 +4,8 @@ import argparse
 import asyncio
 import logging
 
+import psycopg
+
 from psycopg_async_listen.execute import listen_for_notifications
 from psycopg_async_listen.config import CONFIG
 
@@ -16,7 +18,12 @@ def execute():
     parser.add_argument("channel", help="The channel to listen for notifications on.")
     args = parser.parse_args()
 
-    asyncio.run(listen_for_notifications(args.channel, asyncio.Queue()))
+    try:
+        asyncio.run(listen_for_notifications(args.channel, asyncio.Queue()))
+    except KeyboardInterrupt:
+        _log.debug("Exiting application")
+    except psycopg.OpertionalError as e:
+        _log.error("An error occurred: %s, disconnecting", e)
 
 
 if __name__ == "__main__":

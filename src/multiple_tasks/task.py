e77549bc9c3efee527218791e8109c2fea6a0609
Michael <mike@hematite.tech>
2024-09-29 13:56:25 -0700

    added tasks for multiple_tasks package to get to 98% coverage

--- src/multiple_tasks/task.py
+++ src/multiple_tasks/task.py
@@ -1,4 +1,5 @@
 """Tasks to run concurrently."""
+
 import asyncio
 import datetime
 import logging
@@ -14,29 +15,33 @@ _log = logging.getLogger(__name__)
 # We're going to expirment with a Pomodoro Timer. A technique for working
 # in short bursts of time and taking breaks. We'll use asyncio to run
 # multiple timers concurrently.
-# 
+#
 # To allow expirmenting with inheritence, we'll create a base abastract class
 # that defines the interface for a timer. Then we'll create a concrete class
 # that implements the timer logic. Finally, we'll create a class that
 # manages multiple timers concurrently.
 
+
 # Base class for a timer
 class AbstractTimer(ABC):
     _completion_template: str
+
     def __init__(self, duration: int):
         """Initalize a timer with duration in minutes.
 
         :param duration: The duration in minutes
         """
         self._duration = duration
-    
+
     @abstractmethod
     async def __call__(self) -> tuple[str, datetime.datetime, datetime.timedelta]:
         """Run the timer.
-        
+
         :return: A tuple with the name of the timer, the start time, and the duration
         """
         pass
+
+
 # We are going to cratea NamedTimer that will take a name in addition to the duration
 # and will log the name of the timer when it starts and completes.
 class NamedTimer(AbstractTimer):
@@ -48,45 +53,39 @@ class NamedTimer(AbstractTimer):
         """
         super().__init__(duration)
         self._name = name
+
     async def __call__(self) -> tuple[str, datetime.datetime, datetime.timedelta]:
         """Run the timer.
-        
+
         :return: A tuple with the name of the timer, the start time, and the duration
         """
         _log.info(f"Starting {self._name} for {self._duration} minutes")
         start_time = datetime.datetime.now()
-        return self._name, start_time, start_time, datetime.timedelta(minutes=self._duration)
+        await asyncio.sleep(self._duration * 60)
+        _log.info(f"{self._name.title()}'s {self._completion_template}")
+        return self._name, start_time, datetime.timedelta(minutes=self._duration)
+
 
 # Concrete class for Work session, configurable with duration
 class WorkTimer(NamedTimer):
     _completion_template = "work session complete"
-    async def __call__(self):
-        """Run the work session."""
-        super().__call__()
-        _log.debug(f"Starting work session for {self._duration} minutes")
-        await asyncio.sleep(self._duration * 60)
-        _log.info(f"{self._name.title()}'s {self._completion_template}")
-        return self._name, 
-    
+
+
 # Concrete class for Break session, configurable with duration
 class BreakTimer(NamedTimer):
     _completion_template = "break session complete"
-    async def __call__(self):
-        """Run the break session."""
-        self.__call__()
-        _log.debug(f"Starting break session for {self._duration} minutes")
-        await asyncio.sleep(self._duration * 60)
-        _log.info(f"{self._name.title()}'s {self._completion_template}")
+
 
 # To manager multiple timers concurrently, let's think of a scenario like a university library
 # where students are studying, taking breaks, and working on assignments:
 #
 # 1. Harneet start studying at 4:05 PM and uses 20 minutes work sessions and 5 minutes breaks.
 # 2. Ananya starts studying at 4:10 PM and uses 25 minutes work sessions and 5 minutes breaks.
-# 
+#
 # We'll create a coroutine that manages these timers concurrently. With Harneet starting first, and Ananya starting 5 minutes later.
-# We'll not use asyncio.gather() to run the timers concurrently (because it hardly ever the right choice). Instead, we'll use 
+# We'll not use asyncio.gather() to run the timers concurrently (because it hardly ever the right choice). Instead, we'll use
 # asyncio.create_task() to run the timers concurrently.
+
 
 # We are creaating async iterator that will yield alternating work and break timers
 # for each student
@@ -102,7 +101,7 @@ class SessionIterator:
         self._work_duration = work_duration
         self._break_duration = break_duration
         self._next_timer_is_work = True
-    
+
     def __aiter__(self):
         return self
 
@@ -119,6 +118,7 @@ class SessionIterator:
             self._next_timer_is_work = True
             return BreakTimer(self._name, self._break_duration)
 
+
 # Then, we are going to create a coroutine that will:
 # - yield coroutine from async iterators
 # - start a timer
@@ -131,12 +131,12 @@ async def run_session(name: str, work_duration: int, break_duration: int):
     :param work_duration: The duration of the work session in minutes
     :param break_duration: The duration of the break session in minutes
     """
-    session = SessionIterator(name, work_duration, break_duration)
-    async for timer in session:
-        await timer()
+    session = [await timer() async for timer in SessionIterator(name, work_duration, break_duration)]
+    return session
+
 
 # Coroutine to run multiple session concurrently
-async def run_sessions():
+async def run_sessions(delay=float):
     """Run multiple sessions concurrently."""
     harneet = run_session("Harneet", 20, 5)
     ananya = run_session("Ananya", 25, 5)
@@ -144,29 +144,23 @@ async def run_sessions():
     # create tasks for each session, include the 5 minute delay for Ananya
     # include  try statement to catch the CancelledError or KeyboardInterrupt
     try:
+        _log.info("Starting Harneet's session")
         harneet_task = asyncio.create_task(harneet)
-        await asyncio.sleep(5 * 60)
+        _log.info(f"Waiting for {delay} minutes before starting Ananya's session")
+        await asyncio.sleep(delay * 60)
+        _log.info("Starting Ananya's session")
         ananya_task = asyncio.create_task(ananya)
 
         # wait for both tasks to complete
         await harneet_task
         await ananya_task
-    except (asyncio.CancelledError, KeyboardInterrupt) as e:
+    except (asyncio.CancelledError, KeyboardInterrupt):
         harneet_task.cancel()
         ananya_task.cancel()
         await asyncio.gather(harneet_task, ananya_task, return_exceptions=True)
 
-        # await for cancellation to complete
-        await harneet_task
-        await ananya_task
 
-        if e is asyncio.CancelledError:
-            raise e
-        else:
-            _log.info("Tasks cancelled by user")
-    
 # Run the multiple sessions concurrently
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     asyncio.run(run_sessions())
-    
--- src/test/test_task.py
+++ src/test/test_task.py
@@ -1,9 +1,30 @@
 """Tests for muliple tasks's task module."""
+
 import asyncio
+
+from unittest import mock
 
 import pytest
 
-from multiple_tasks.task import SessionIterator, run_session, run_sessions, WorkTimer, BreakTimer
+from multiple_tasks.task import (
+    SessionIterator,
+    run_session,
+    run_sessions,
+    WorkTimer,
+    BreakTimer,
+    AbstractTimer,
+    NamedTimer,
+)
+
+
+@pytest.mark.asyncio
+async def test_abstract_class_error():
+    m_duration = 1
+
+    with pytest.raises(TypeError):
+        timer = AbstractTimer(m_duration)
+        await timer()
+
 
 # test work timer, set duration to 1 second
 @pytest.mark.asyncio
@@ -23,16 +44,17 @@ async def test_work_timer(m_timer_type, m_timer_name):
     task = asyncio.create_task(timer())
     await task
 
-    assert task.done()    
+    assert task.done()
 
     assert timer._name == m_timer_name
     assert timer._duration == 0.1
+
 
 # test session iterator
 @pytest.mark.asyncio
 async def test_session_iterator():
     """Test the session iterator."""
-    m_timer_name = "Test" 
+    m_timer_name = "Test"
 
     # create session iterator
     session = SessionIterator(m_timer_name, 0.1, 0.1)
@@ -43,11 +65,14 @@ async def test_session_iterator():
     assert timer._name == m_timer_name
     assert timer._duration == 0.1
 
+    assert session.__aiter__() == session
+
     # get break timer
     timer = await session.__anext__()
     assert isinstance(timer, BreakTimer)
     assert timer._name == m_timer_name
     assert timer._duration == 0.1
+
 
 # test run session, with work duration of 1 second and break duration of 1 second
 @pytest.mark.asyncio
@@ -57,14 +82,62 @@ async def test_run_session():
     m_work_duration = 0.1
     m_break_duration = 0.1
 
-    # create task for run session
-    task = asyncio.create_task(run_session(m_name, m_work_duration, m_break_duration))
-    await task
+    class TestTimer(NamedTimer):
+        _completion_template = "test session complete"
 
-    # sleep for 1 second
-    await asyncio.sleep(1)
+    class TestSessionIterator(SessionIterator):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            # in test classes, we assert object variables
+            assert self._name == m_name
+            assert self._work_duration == m_work_duration
+            assert self._break_duration == m_break_duration
+            self._count = 0
 
-    task.cancel()
-    await task
+        async def __anext__(self):
+            if self._count == 0:
+                self._count += 1
+                return TestTimer(self._name, self._work_duration)
+            else:
+                raise StopAsyncIteration
 
-    assert task.done()
+    with mock.patch("multiple_tasks.task.SessionIterator", new=TestSessionIterator):
+        # create task for run session
+        task = asyncio.create_task(run_session(m_name, m_work_duration, m_break_duration))
+
+        await asyncio.sleep(0.1)
+        await asyncio.wait_for(task, timeout=10)
+
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+
+        assert task.done()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "m_exception, should_cancel",
+    [
+        (asyncio.CancelledError, True),
+        (None, False),
+    ],
+)
+async def test_run_sessions(m_exception, should_cancel, caplog):
+    """Test the run sessions."""
+    # set log level to info
+    caplog.set_level(10)
+    with (
+        mock.patch("multiple_tasks.task.run_session") as m_run_session,
+    ):
+        if should_cancel:
+            m_run_session.side_effect = m_exception
+
+        _ = await run_sessions(0.1)
+
+        assert m_run_session.call_count == 2
+        assert "Starting Harneet's session" in caplog.text
+        assert "Waiting for 0.1 minutes before starting Ananya's session" in caplog.text
+        assert "Starting Ananya's session" in caplog.text
